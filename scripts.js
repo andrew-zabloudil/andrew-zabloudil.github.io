0960e3f5b4760a1c17b8526ba2a52aea7cae2db1
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-07-20 23:47:03 -0500

    Add initial splash screen, update cards

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-4rem";
+  }
+}
