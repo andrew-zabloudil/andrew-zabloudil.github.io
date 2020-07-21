85f91a7ffb8950231e19e9de311a28cf860ac0f1
Andrew Zabloudil <andrew.zabloudil@gmail.com>
2020-07-21 00:16:44 -0500

    Add demo gifs of projects

--- scripts.js
+++ scripts.js
@@ -19,3 +19,35 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "-4rem";
   }
 }
+
+var bbModal = document.getElementById("boogabotModal");
+var pfModal = document.getElementById("pathfinderModal");
+var trModal = document.getElementById("tetrosModal");
+
+var demos = document.getElementsByClassName("demo");
+
+var bbGif = document.getElementById("boogabotDemo");
+var pfGif = document.getElementById("pathfinderDemo");
+var trGif = document.getElementById("tetrosDemo");
+
+demos[0].onclick = function () {
+  bbModal.style.display = "block";
+};
+demos[1].onclick = function () {
+  pfModal.style.display = "block";
+};
+demos[2].onclick = function () {
+  trModal.style.display = "block";
+};
+
+var span = document.getElementsByClassName("close");
+
+span[0].onclick = function () {
+  bbModal.style.display = "none";
+};
+span[1].onclick = function () {
+  pfModal.style.display = "none";
+};
+span[2].onclick = function () {
+  trModal.style.display = "none";
+};
